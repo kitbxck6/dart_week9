b532938efcb9da1f07bdf7070fa1f48647e06cba
kitbxck6 <kittipong8426@gmail.com>
2022-07-11 10:47:29 +0700

    first commit

---
+++ bin/dart_week9.dart
@@ -0,0 +1,33 @@
+void main() {
+  
+int zodiac =2002;
+if(zodiac == 1996 || zodiac == 2008)
+{
+ print("Zodiac:Rat");
+} else if(zodiac == 1997 || zodiac == 2009){
+ print("Zodiac:Oxen");
+} else if(zodiac == 1998 || zodiac == 2010){
+ print("Zodiac:Tiger");
+} else if(zodiac == 1999 || zodiac == 2011){
+ print("Zodiac:Rabbit");
+} else if(zodiac == 2000 || zodiac == 2012){
+ print("Zodiac:Dragon");
+} else if(zodiac == 2001 || zodiac == 2013){
+ print("Zodiac:Snake");
+} else if(zodiac == 2002 || zodiac == 2014){
+ print("Zodiac:Horse");
+} else if(zodiac == 2003 || zodiac == 2015){
+ print("Zodiac:Sheep");
+} else if(zodiac == 2004 || zodiac == 2016){
+ print("Zodiac:Monkey");
+} else if(zodiac == 2005 || zodiac == 2017){
+ print("Zodiac:Rooster");
+} else if(zodiac == 2006 || zodiac == 2018){
+ print("Zodiac:Dog");
+} else if(zodiac == 2007 || zodiac == 2019){
+ print("Zodiac:Pig");
+} else{
+  print("Zodiac:Error");
+}
+
+}
